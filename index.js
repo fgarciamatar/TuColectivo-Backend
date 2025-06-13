f6124fe422d6f9fd27d851c7bcdc8e8531b59953
Mateo <mateo050722@gmail.com>
2025-06-13 16:24:39 -0300

    cambios

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 require("dotenv").config();
 const { connectDB } = require("./src/models/database");
-// const cors = require("cors");
+const cors = require("cors");
 // const { connectDB } = require("./src/models/database");
 
 const routes = require("./src/routes/index");
 
-// app.use(cors({
-//   origin: process.env.FRONT,
-//    credentials: true,
-// }))
 const app = express();
+app.use(cors({
+  origin: '*', // o especificá tu IP: 'http://192.168.0.123:3000'
+  methods: ['GET', 'POST'],
+}));
+
 app.use(express.json());
 app.use("/", routes);
 
 const PORT = process.env.PORT || 3002;
+const IP = "192.168.1.9"
 
-app.listen(PORT, async () => {
-  await connectDB(); // 📡 Acá conectás
+
+
+app.listen(3002,IP, async () => {
+   await connectDB(); // 📡 Acá conectás
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
+
